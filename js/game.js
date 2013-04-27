2d2e92ef4706f426c5b86bb714faf705177d6c2c
Olivier Yiptong <olivier@olivieryiptong.com>
2013-04-27 15:59:51 -0400

    minigame demo code

--- js/game.js
+++ js/game.js
@@ -8,9 +8,35 @@
     }
   };
 
+  /*
+  function Minigame() {
+    this.document = null;
+  }
+
+  Minigame.prototype.init = function(iframe) {
+    this.document = iframe.contentDocument;
+    console.log(this.document);
+    this.gameArea = this.document.getElementById("gameArea");
+  }
+
+  Minigame.prototype.jiggle = function() {
+    header = this.document.createElement("h1");
+    header.innerHTML = "HOWDY!";
+    this.gameArea.appendChild(header);
+  }
+  */
+
   window.onload = function (e) {
     var iframeContainer = document.querySelector('#iframe-container');
     iframeContainer.appendChild(util.createIframe('scenes/cell.html'));
+
+    /*
+    var minigame = util.createIframe('scenes/minigame_test.html');
+    iframeContainer.appendChild(minigame);
+    var game = new Minigame();
+    game.init(minigame);
+    game.jiggle();
+    */
   };
 
   window.game = {
