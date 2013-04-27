2dfa15e5f9ffac5ea4cd4c97e7af36cf844d1692
Gino Fazari <fazari@gmail.com>
2013-04-27 16:43:35 -0400

    sounds: play pause resume functions
    also added autoplay and loop vars

--- js/audio.js
+++ js/audio.js
@@ -3,18 +3,47 @@ AudioController ={
       "ambient":{
         id:"ambient",
         src:"assets/audio/ambient.mp3|assets/audio/ambient.ogg",
-        instance:null
+        autoplay:true,
+        loop:true,
+        instance:null,
+        loaded:false
       }
     },
     loadSounds: function(){
       createjs.Sound.addEventListener("loadComplete", createjs.proxy(this.loadHandler, this));
-      createjs.Sound.registerSound(this.sounds["ambient"].src, this.sounds["ambient"].id);  
+      createjs.Sound.registerSound(this.sounds["ambient"].src, this.sounds["ambient"].id); 
     },
     loadHandler: function(e) {
-      //play(src,  [interrupt="none"], [delay=0], [offset=0], [loop=0], [volume=1], [pan=0])
-      this.sounds[e.id] = createjs.Sound.play(this.sounds[e.id].id,"none", 0, 0.25, -1, 1);
-      this.sounds[e.id].addEventListener("complete", createjs.proxy(this.handleComplete, this));
+      console.log("loaded",e.id," autoplay?",this.sounds[e.id].autoplay)
+      if (this.sounds[e.id].autoplay){
+        console.log("play",e.id)
+        this.sounds[e.id].loaded = true;
+        this.playSound(e.id, this.sounds[e.id].loop);
+      }
+    },
+    playSound: function(id, loop){
+      if (this.sounds[id] && this.sounds[id].loaded){
+        //play(src,  [interrupt="none"], [delay=0], [offset=0], [loop=0], [volume=1], [pan=0])
+        this.sounds[id].instance = createjs.Sound.play(this.sounds[id].id,"none", 0, 0.25, loop?-1:0, 1)
+      }
+    },
+    pauseSound:function(id){
+      if (this.sounds[id] && this.sounds[id].instance){
+        this.sounds[id].instance.pause();
+      }
+    },
+    resumeSound:function(id){
+      if (this.sounds[id] && this.sounds[id].instance){
+        this.sounds[id].instance.resume();
+      }
+    },
+    getSoundInstance:function(id){
+      if (this.sounds[id] && this.sounds[id].instance){
+        return this.sounds[id].instance;
+      }
     }
+
+
 };
 
 this.AudioController.loadSounds();
