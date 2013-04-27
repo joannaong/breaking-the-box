354bd69558b7250026094846fcfd6dd3ab17231a
secretrobotron <secretrobotron@gmail.com>
2013-04-27 15:17:55 -0400

    setup for game

---
+++ js/game.js
@@ -0,0 +1,16 @@
+(function(){
+  
+  var util = {
+    createIframe: function (url) {
+      var iframe = document.createElement('iframe');
+      iframe.src = url;
+      return iframe;
+    }
+  };
+
+  window.onload = function (e) {
+    var iframeContainer = document.querySelector('#iframe-container');
+    iframeContainer.appendChild(util.createIframe('scenes/cell.html'));
+  };
+
+}());
