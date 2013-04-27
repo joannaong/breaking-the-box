b897642a5fd2a5ab5e25115498d40133c7104128
secretrobotron <secretrobotron@gmail.com>
2013-04-27 15:50:49 -0400

    loading infra

--- js/scene.js
+++ js/scene.js
@@ -2,14 +2,29 @@
   
   var _cubemap;
   var _cubemapDiv;
+  var _onSceneReadyCallback;
+  var _loaded = false;
 
   window.onload = function (e) {
     _cubemapDiv = document.querySelector('#cubemap');
+    _loaded = true;
+
+    if (_onSceneReadyCallback) {
+      _onSceneReadyCallback();
+    }
   };
 
   window.scene = {
+    onready: function (callback) {
+      if (_loaded) {
+        setTimeout(function(){
+          callback();
+        }, 0);
+      }
+      _onSceneReadyCallback = callback;
+    },
     addHotRegion: function (definition, mouseOverCallback, mouseOutCallback) {
-      
+
     },
     createCubemap: function (assetPrefix) {
       _cubemap = new Cubemap(assetPrefix, ".png", "cubemap", {
