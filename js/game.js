d6a7bb88ab02bf65c2c59670d4c786cf3dc68f07
Anthony Pilger <anthony.pilger@gmail.com>
2013-04-28 11:52:57 -0400

    fixed cubemap back plane and images

--- js/game.js
+++ js/game.js
@@ -26,19 +26,41 @@
   }
   */
 
-  window.onload = function (e) {
-    var iframeContainer = document.querySelector('#iframe-container');
-    iframeContainer.appendChild(util.createIframe('scenes/cell.html'));
+  var bInitalState  = true;
 
-    /*
-    var minigame = util.createIframe('scenes/minigame_test.html');
-    iframeContainer.appendChild(minigame);
-    var game = new Minigame();
-    game.init(minigame);
-    game.jiggle();
-    */
+  window.onload = function (e) {
+    this.game.showLowerScene();
   };
 
   window.game = {
+    showCellScene: function(){
+      bInitalState  = true;
+      var iframeContainer = document.querySelector('#iframe-container');
+      var cellFrame = util.createIframe('scenes/cell.html');
+      $(iframeContainer).fadeOut(function(){
+        $(iframeContainer).empty();  
+        iframeContainer.appendChild(cellFrame);
+        $(iframeContainer).fadeIn();
+      });
+    },
+    showLowerScene: function(){
+      bInitalState  = false;
+      var iframeContainer = document.querySelector('#iframe-container');
+      var cellFrame = util.createIframe('scenes/lower.html');
+      $(iframeContainer).fadeOut(function(){
+        $(iframeContainer).empty();  
+        iframeContainer.appendChild(cellFrame);
+        $(iframeContainer).fadeIn();
+      });
+    },
+
+    onMiniGameOver:function()
+    {
+      if(bInitalState ){
+        this.showLowerScene();
+      }else{
+
+      }
+    }
   };
 }());
